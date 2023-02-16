49200d8cd8b2b4f9066a2287f1188293cc3e6d4a
mabdulwahid008 <m.abdulwahid008@gmail.com>
2023-02-16 23:04:40 +0500

    setting up project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import AdminLayout from "layouts/Admin.js";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Login from 'views/Login';
+
+function App() {
+    const token = localStorage.getItem('token')
+    
+    if(!token)
+        return (
+            <Login />
+        )
+    else
+        return (
+            <Switch>
+                <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+                <Redirect to="/admin/dashboard" />
+            </Switch>
+        )
+}
+
+export default App
---
+++ src/views/Login.js
@@ -0,0 +1,60 @@
+import  React, { useState } from 'react'
+import { Button, Card, CardBody, CardHeader, CardTitle, Col, Form, FormGroup, Input, Row } from 'reactstrap'
+
+function Login() {
+    const [loading, setloading] = useState(false)
+    const [logins, setLogins] = useState({email: '', password: ''})
+
+    const onChange = ( e ) => {
+        setLogins({...logins, [e.target.name]: e.target.value})
+        console.log(logins);
+    }
+
+    const  onSubmit = ( e ) => {
+        e.preventDefault();
+
+        localStorage.setItem('token', "true")
+
+        window.location.reload(true)
+    }
+  return (
+    <div>
+    <Row style={{display:'flex', margin:0, justifyContent: 'center', alignItems:'center', overflow: 'hiden', height: '100vh', width: '100vw', background: '#f4f3ef'}}>
+        <Col md="4">
+            <Card>
+                <CardHeader>
+                    <CardTitle tag="h4">Login</CardTitle>
+                </CardHeader>
+                <CardBody>
+                    <Form onSubmit={onSubmit}>
+                        <Row>
+                            <Col md="12">
+                                <FormGroup>
+                                    <label>Username</label>
+                                    <Input placeholder="Username" type="text" name='email' value={logins.email} required onChange={onChange}/>
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md="12">
+                                <FormGroup>
+                                    <label>Password</label>
+                                    <Input placeholder="Password" type="password"  name="password" value={logins.password} required onChange={onChange}/>
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <div style={{padding:'0px 15px'}}>
+                                <Button color="primary" type="submit" disabled={loading? true: false}>{loading? 'Please Wait...' :  'Login'}</Button>
+                            </div>
+                        </Row>
+                    </Form>
+                </CardBody>
+            </Card>
+        </Col>
+    </Row>
+</div>
+  )
+}
+
+export default Login
