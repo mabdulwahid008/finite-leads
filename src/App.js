e8c66ff13f3202324f4710d0ed18392c623fc80b
mabdulwahid008 <m.abdulwahid008@gmail.com>
2023-02-24 20:15:46 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import AdminLayout from "layouts/Admin.js";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 import Login from 'views/Login';
 import { ToastContainer, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+    const history = useHistory()
+    history.push('/dashboard')
+
     const token = localStorage.getItem('token')
     if(!token)
         return (
             <>
-                <Login />
+                <Switch>
+                    <Route path="/dashboard" render={ (props) =>  <Login />} />
+                </Switch>
                 <ToastContainer position="top-right" autoClose={2000} hideProgressBar={false} transition={Slide} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss theme="light" />
             </>
         )
         return (
             <>
                 <Switch>
-                    <Route path="/" render={(props) => <AdminLayout {...props} />} />
-                    {/* <Redirect to="/dashboard" /> */}
+                    <Route path="/" render={ (props) =>  <AdminLayout {...props} /> } />
                 </Switch>
                 <ToastContainer position="top-right" autoClose={2000} hideProgressBar={false} transition={Slide} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss theme="light" />
             </>
@@ -26,3 +30,4 @@ function App() {
 }
 
 export default App
+
