e67a9887d1fce9b77c6f0f2e6bd91164543e3475
mabdulwahid008 <m.abdulwahid008@gmail.com>
2023-03-20 23:04:13 +0500

    commit

--- finitelead sales/backend_sql/middleware/masterOrAdminAuthorization.js
+++
@@ -1,9 +0,0 @@
-
-const masterOrAdminAuthorization = (req, res, next) => {
-    if(req.user_role !== 5 && req.user_role !== 3)
-        return res.status(401).json({message: "Unauthorized Attempt"})
-    else
-        next()
-}
-
-module.exports = masterOrAdminAuthorization
