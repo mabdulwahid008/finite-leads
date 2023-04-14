cf20d29afab489133f9081cb36c0bba1ce70240a
mabdulwahid008 <m.abdulwahid008@gmail.com>
2023-04-14 20:38:22 +0500

    commit

--- backend_sql/routes/Leads.js
+++ backend_sql/routes/Leads.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const db = require('../db');
+const authorization = require('../middleware/authorization');
+const masterOrAdminAuthorization = require('../middleware/masterOrAdminAuthorization');
 const router = express.Router();
 
 
+// for form - lead posting
 router.post('/', async(req, res) => {
     const { lead_type, working_status, fname, lname, address, state, zip_code, phone, beds, baths, price, additional_info, agentName } = req.body;
     try {
@@ -10,6 +13,33 @@ router.post('/', async(req, res) => {
             lead_type, working_status, fname, lname, address, state, zip_code, phone, beds, baths, price, additional_info, agentName
         ])
         return res.status(200).json({message: 'Lead added successfully.'})
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({message: 'Server Error'})
+    }
+})
+
+// for getting leads
+router.get('/', authorization, async(req, res) => {
+    try {
+        const leads = await db.query('SELECT * FROM leads')
+        return res.status(200).json(leads.rows)
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({message: 'Server Error'})
+    }
+})
+
+// for assigning lead to real estate agents
+router.post('/', authorization, masterOrAdminAuthorization, async(req, res) => {
+    const { lead_id, agents } = req.body;
+    try {
+        for (let i = 0; i < agents.length; i++) {
+            await db.query('INSERT INTO LEAD_ASSIGNED_TO(lead_id, realEstateAgent_id) VALUES($1, $2)',[
+                lead_id, agents[i]
+            ])
+        }
+        return res.status(200).json({message: 'Lead assigned successfully.'})
     } catch (error) {
         console.log(error.message);
         return res.status(500).json({message: 'Server Error'})
