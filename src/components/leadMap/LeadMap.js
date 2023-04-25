144fd68f0bf1ed463a0582c1ff10460c4be3175b
mabdulwahid008 <m.abdulwahid008@gmail.com>
2023-04-25 21:55:36 +0500

    commit

--- src/components/leadMap/LeadMap.js
+++ src/components/leadMap/LeadMap.js
@@ -1,32 +1,47 @@
-import React, { useEffect, useRef, useState } from 'react'
-import mapboxgl from 'mapbox-gl'
-import ReactMapGL from 'react-map-gl'
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFiZHVsd2FoaWQwMDgiLCJhIjoiY2xnbzBsamkxMDFiZzNlbXZxaGlkdG5tZCJ9.fjlXr72SGHstXogWaLPd4A'
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
 
 function LeadMap() {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
-    const [zoom, setZoom] = useState(9);
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyBDQkVQ5dTM8lJ72epfylv0yCxNUVB8Ukw"
+  })
 
-    const [viewport, setViewport] = useState({
-        latitude:42.35,
-        longitude:-70.9,
-        width: '100vw',
-        height: '400px',
-        zoom: 10
-    })
+  const [map, setMap] = React.useState(null)
 
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
 
-  return (
-    <div>
-        {/* <ReactMapGL {...viewport} mapboxApiAccessToken='pk.eyJ1IjoibWFiZHVsd2FoaWQwMDgiLCJhIjoiY2xnbzBsamkxMDFiZzNlbXZxaGlkdG5tZCJ9.fjlXr72SGHstXogWaLPd4A'
-        ></ReactMapGL> */}
-        <div ref={mapContainer} className="map-container" style={{height: 400,}}/>
-    </div>
-  )
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
 }
 
-export default LeadMap
+export default React.memo(LeadMap)
