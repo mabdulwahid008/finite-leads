89dc908e065ccee8928c97b99501ff65db106424
mabdulwahid008 <m.abdulwahid008@gmail.com>
2023-07-11 20:56:45 +0500

    commit

--- backend_sql/routes/file.js
+++ backend_sql/routes/file.js
@@ -9,7 +9,8 @@ const bcrypt = require('bcrypt')
 
 const upload = multer();
 
-router.get('/', upload.single('csvFile'), async(req, res) => {
+// for re agents
+router.post('/agents', upload.single('csvFile'), async(req, res) => {
 
     const results = [];
     const csvString = req.file.buffer.toString(); 
@@ -63,6 +64,56 @@ router.get('/', upload.single('csvFile'), async(req, res) => {
                 console.error(error.message);
                 res.status(500).send('Internal Server Error');
             });
+})
+
+
+
+// for leads
+router.post('/leads', upload.single('csvFile'), async(req, res) => {
+    const results = [];
+    const csvString = req.file.buffer.toString(); 
+  
+    const stream = Readable.from(csvString)
+        .pipe(csv({transform: (value, header) => (header === 'Phone Number' ? value.toString() : value) }))
+        .on('headers', (headers) => {
+            // Remove empty headers
+            const validHeaders = headers.filter((header) => header.trim() !== '');
+            stream.headers = validHeaders.map((header) => header.replace(/ /g, '_')); // Convert spaces to underscores
+          })
+          .on('data', (data) => {
+            const formattedData = {};
+      
+            // Map the values to the valid headers with converted keys
+            stream.headers.forEach((header) => {
+              formattedData[header] = data[header];
+            });
+      
+            results.push(formattedData);
+          })
+          .on('end', async() => {
+            for (let i = 0; i < results.length; i++) {
+                let Phone_Number;
+                for (let j = 0; j < results[i].Phone_Number.length; j++) {
+                    Phone_Number = results[i].Phone_Number.replace(/[^0-9]/g, '')
+                }
+                results[i].Phone_Number = Phone_Number
+                results[i]['Currently_working_with_an_agent:'] = results[i]['Currently_working_with_an_agent:'].toLowerCase() === 'no' ? 0 : 1;
+                results[i]['I_want_to:'] = results[i]['I_want_to:'].toLowerCase() === 'sell' ? 0 : 1;
+    
+            }
+
+            for (let i = 0; i < results.length; i++) {
+                // console.log(results[i]);
+                await db.query('INSERT INTO leads(lead_type, working_status, fname, lname, address, state, zip_code, phone, beds, baths, price, additional_info, created_on, agentName) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)',[
+                    results[i]['I_want_to:'], results[i]['Currently_working_with_an_agent:'], results[i].First_Name, results[i].Last_Name, results[i].Address, results[i].State, results[i].zip_code, results[i].Phone_Number, results[i].Beds, results[i].Baths, results[i].price ? results[i].price : 0, results[i].Additional, dateWithoutTime, results[i].Agent_Name
+                ])
+            }
+            return res.status(200).json(results)
+          })
+          .on('error', (error) => {
+            console.error(error.message);
+            res.status(500).send(error.message);
+        });
 })
 
 module.exports = router
